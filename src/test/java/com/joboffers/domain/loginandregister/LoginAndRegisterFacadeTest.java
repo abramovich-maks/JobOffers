9a562417dedc4c9d92e03c0136b50efd23b0e429
abramovich-maks <maks.abramovich.1995@mail.ru>
2025-10-19 19:01:23 +0200

    created facades and packages modules and hexagonal (domain, infrastructure)

---
+++ src/test/java/com/joboffers/domain/loginandregister/LoginAndRegisterFacadeTest.java
@@ -0,0 +1,11 @@
+package com.joboffers.domain.loginandregister;
+
+import org.junit.jupiter.api.Test;
+
+class LoginAndRegisterFacadeTest {
+
+    @Test
+    public void f() {
+
+    }
+}
---
+++ src/test/java/com/joboffers/domain/offer/OfferFacadeTest.java
@@ -0,0 +1,12 @@
+package com.joboffers.domain.offer;
+
+import org.junit.jupiter.api.Test;
+
+class OfferFacadeTest {
+
+    @Test
+    public void f() {
+
+    }
+
+}
