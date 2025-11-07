89877f333536a4b388b9fb18210ecc004d2ad5a9
abramovich-maks <maks.abramovich.1995@mail.ru>
2025-11-07 12:37:06 +0100

    implement /token endpoint for user authentication

---
+++ src/main/java/com/joboffers/infrastructure/security/jwt/JwtAuthenticatorFacade.java
@@ -0,0 +1,24 @@
+package com.joboffers.infrastructure.security.jwt;
+
+import com.joboffers.infrastructure.token.controller.dto.JwtResponseDto;
+import com.joboffers.infrastructure.token.controller.dto.TokenRequestDto;
+import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import javax.validation.Valid;
+
+@Component
+@AllArgsConstructor
+public class JwtAuthenticatorFacade {
+
+    private final AuthenticationManager authenticationManager;
+
+    public JwtResponseDto authenticateAndGenerateToken(final @Valid TokenRequestDto loginRequest) {
+        Authentication authenticate = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.login(), loginRequest.password()));
+        return JwtResponseDto.builder().build();
+    }
+}
+
